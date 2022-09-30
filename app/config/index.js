39e46531cafb4b243936350f132026b1c6530539
ChiHoa <kyuseishu1593@gmail.com>
2022-09-30 07:51:20 +0700

    abc

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,9 @@
 const config = {
     app: {
         port: process.env.PORT || 3000,
+    },
+    db: {
+        uri: process.env.DATABASE_URI || 'mongodb://localhost:27017/ContactBook'
     }
 }
 
